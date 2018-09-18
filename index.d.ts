3b627f92bbf09944ba2568469245a29a726196aa
Alex Natashkin <natashkinsasha@gmail.com>
2018-09-19 00:05:10 +0300

    v 0.1.2

--- index.d.ts
+++ index.d.ts
@@ -7,7 +7,7 @@ declare namespace router {
     export interface Router {
         (socket: Socket, fn: (err?: any) => void): void
 
-        use(pathOrFunc: string | ((io: Server, socket: Socket, path: string, params: any[], ack: ((...params: any[]) => any) | undefined, err: any) => any), func?: (io: Server, socket: Socket, path: string, params: any[], ack: (params?: any[]) => any, next: (err?: any) => any) => any): Router;
+        use(pathOrFunc: string | ((io: Server, socket: Socket, err: any, ack: ((...params: any[]) => any) | undefined) => any), func?: (io: Server, socket: Socket, path: string, params: any[], ack: ((...params: any[]) => any) | undefined, next: (err?: any) => any) => any): Router;
 
         onConnect(func: (io: Server, socket: Socket, next: (err?: any) => any) => any): Router;
 
